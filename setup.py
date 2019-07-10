75f89c41fd6d467703afd929ee6508b24bd2cbf1
Brian Chao <brianchao@Brians-MacBook-Pro.local>
2019-07-11 01:39:15 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 setup(
     name = 'griddy',
+    author = "Mckinsey666",
+    description = "Generate CSS grid-layouts FAST.",
     version = '0.1.0',
     packages = ['griddy'],
+    url = "https://github.com/Mckinsey666/griddy",
+    keywords = "html css javascript grid-layouts flexbox",
     entry_points = {
         'console_scripts': [
             'griddy = griddy.__main__:main'
